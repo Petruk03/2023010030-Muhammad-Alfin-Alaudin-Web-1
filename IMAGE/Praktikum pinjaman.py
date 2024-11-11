f870673fa157efa69caeee81523945d6fba6219b
Muhammad Alfin Alaudin <petrukselluler@gmail.com>
2024-11-11 14:04:16 +0700

    first

---
+++ IMAGE/Praktikum pinjaman.py
@@ -0,0 +1,13 @@
+#Bagian 1
+
+umur = int (input("Masukan usia anda : "))
+pendapatan = int (input("Masukan pendapatan anda :"))
+
+#Hasil keputusan
+if umur >= 21 and pendapatan >= 3000000:
+    print("Pengajuan anda disetujui")
+
+else : 
+    print("Pengajuan tidak memenuhi syarat")
+
+
